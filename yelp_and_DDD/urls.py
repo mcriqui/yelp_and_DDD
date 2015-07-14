f799f2fdb3a2250d4e2010ab76ebce5d231f274d
mcriqui <maggie.criqui@gmail.com>
2015-07-14 10:43:20 -0400

    updated style

--- yelp_and_DDD/urls.py
+++ yelp_and_DDD/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+
 urlpatterns = patterns('',
     url(r'^', include('yelp_info.urls')),
     url(r'^admin/', include(admin.site.urls)),
